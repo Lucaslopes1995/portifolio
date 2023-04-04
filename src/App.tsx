cb65e86e5cc02deaa8bc4e5d78bf814296506fd9
Lucas <lucaslopes@gea.inatel.br>
2023-04-04 18:20:22 -0300

    feat- adding customfields to product page details

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Index";
+import GlobalStyle from "./globalStyles";
+
+function App() {
+
+
+	return (
+		<div>
+			<GlobalStyle/>
+			<Header/>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  position: fixed;
+  z-index: 1;
+  display: flex;
+  background-color:green;
+  align-items: center;
+  justify-content: space-around;
+  height: 3rem;
+
+  a{
+    margin-right: 5px
+  }
+`;
+
+
